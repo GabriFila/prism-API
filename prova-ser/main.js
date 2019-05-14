ca723457c2657518130e75e5fb5b71731aba023a
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2019-05-14 19:25:27 +0200

    Added working serial parser

---
+++ prova-ser/main.js
@@ -0,0 +1,13 @@
+const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
+
+const port = new SerialPort("COM4", { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: "dataEnd" }));
+// Read the port data
+port.on("open", () => {
+  console.log("serial port open");
+});
+
+parser.on("data", data => {
+  console.log(JSON.parse(data).Nome);
+});
