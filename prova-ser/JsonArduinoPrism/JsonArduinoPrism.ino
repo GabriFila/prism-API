e1bede3e9462b3b21d0b968615b66e4897cfca7a
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2019-05-15 12:20:00 +0200

    Improved prova-ser

--- prova-ser/JsonArduinoPrism/JsonArduinoPrism.ino
+++ prova-ser/JsonArduinoPrism/JsonArduinoPrism.ino
@@ -1,38 +1,48 @@
 
 #include "ArduinoJson.h"
 
+StaticJsonDocument<256> objRx;
+StaticJsonDocument<256> objTx;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+
   const char* input = "{\"Nome\" : \"Gabriele\", \"Cognome\" : \"Filaferro\"}";
-
   StaticJsonDocument<256> objA;
-  StaticJsonDocument<256> objB;
-
   deserializeJson(objA, input);
 
   const char * nome = objA["Nome"];
 
   const char * cognome = objA["Cognome"];
-  /*
-    Serial.print("Nome: ");
-    Serial.println(nome);
-    Serial.print("Cognome: ");
-    Serial.println(cognome);
-  */
-
-  deserializeJson(objB, Serial);
-  digitalWrite(LED_BUILTIN, LOW);
-
-  if (objB["led"] == true)
-    digitalWrite(LED_BUILTIN,HIGH);
-  serializeJson(objB, Serial);
-
-
-  Serial.print("dataEnd");
+  //serializeJson(objA, Serial);
+  //Serial.print("dataEnd");
 }
 
 void loop() {
+
+  if (Serial.available()) {
+    deserializeJson(objRx, Serial);
+    digitalWrite(LED_BUILTIN, LOW);
+    /*
+      int times = objRx["times"];
+      for (int i = 0; i < times; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay((int)(objRx["delay"])*(i+1));
+      //delay(2000);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay((int)(objRx["delay"])*(i+1));
+      }*/
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(objRx["delay"]);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(objRx["delay"]);
+    objTx["delayRX"] = objRx["delay"];
+    objTx["msg"] = "send" ;
+
+    serializeJson(objTx, Serial);
+    Serial.println();
+  }
 
 }
