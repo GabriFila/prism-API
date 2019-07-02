345d93ae4c9947a649cad328b12b44b3b01333c3
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2019-07-02 18:29:30 +0200

    Added some tests for API

---
+++ prism-touch-bar-v2/test/API/onPrismState/onLasers.test.ts
@@ -0,0 +1,86 @@
+import { server } from "../../../src/server";
+import * as chai from "chai";
+
+import chaiHttp = require("chai-http");
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+describe("on /lasers ", () => {
+  it("PUT correct", done => {
+    chai
+      .request(server)
+      .put("/prismState/lasers/power?waveLength=300")
+      .set("Content-Type", "application/json")
+      .send({
+        newValue: 70
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("newValue");
+        expect(res.body.newValue).to.be.equal(70);
+        done();
+      });
+  });
+
+  it("PUT wrong wave length", done => {
+    chai
+      .request(server)
+      .put("/prismState/lasers/power?waveLength=0")
+      .set("Content-Type", "application/json")
+      .send({
+        newValue: 70
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        expect(res.body.error).to.be.equal("No laser with wavelength 0 nm");
+        done();
+      });
+  });
+
+  it("PUT wrong param", done => {
+    chai
+      .request(server)
+      .put("/prismState/lasers/45?waveLength=300")
+      .set("Content-Type", "application/json")
+      .send({
+        newValue: 70
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        expect(res.body.error).to.be.equal("45 is not a valid url parameter");
+        done();
+      });
+  });
+
+  it("PUT invalid isOn value", done => {
+    chai
+      .request(server)
+      .put("/prismState/lasers/isOn?waveLength=300")
+      .set("Content-Type", "application/json")
+      .send({
+        newValue: 66
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        expect(res.body.error).to.be.equal("66 is not boolean");
+        done();
+      });
+  });
+
+  it("PUT invalid power value", done => {
+    chai
+      .request(server)
+      .put("/prismState/lasers/power?waveLength=300")
+      .set("Content-Type", "application/json")
+      .send({
+        newValue: true
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        expect(res.body.error).to.be.equal("true is not a number");
+        done();
+      });
+  });
+});
