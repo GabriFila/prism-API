542da13b9e77808567df72ff35c45700660084c1
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2019-07-10 14:07:48 +0200

    Made some changes

--- prism-touch-bar-v2/src/middlewares/routes/updates-route.ts
+++ prism-touch-bar-v2/src/middlewares/routes/updates-route.ts
@@ -9,7 +9,7 @@ updates.get("/", (req, res) => {
   res.writeHead(200, {
     "Content-Type": "text/event-stream",
     "Cache-Control": "no-cache",
-    Connection: "keep-alive"
+    "Connection": "keep-alive"
   });
 
   observer.subscribe(this, "update-to-UI", (who: any, resource: Resource) => {
