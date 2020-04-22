edc604dac9fe748f1564f8bc8f2903bd37ab7e78
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2020-04-22 14:18:37 +0200

    Removed ts-dist

--- ts-dist/src/UI/waitConnectUI.js
+++
@@ -1,4 +0,0 @@
-const source = new EventSource("/updates");
-//set up SSE and wait for 'micro-connected' event
-source.addEventListener("micro-connected", () => document.location.reload());
-//# sourceMappingURL=waitConnectUI.js.map
